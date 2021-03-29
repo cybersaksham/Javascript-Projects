44488883bfc69d391b5d7edbe9a3b95ac19b4f9e
cybersaksham <2020UCP1801@mnit.ac.in>
2021-03-29 14:19:38 +0530

    clock app update 1.0.0

--- P8_clock/JS/index.js
+++ P8_clock/JS/index.js
@@ -3,12 +3,19 @@ let hour = document.getElementById('hour');
 let minute = document.getElementById('minute');
 let second = document.getElementById('second');
 
+let time = document.getElementById('time');
+
 // Starting
 showClock();
 
 // Each second
 setInterval(() => {
     showClock();
+    let tick = new Audio('Gallery/tick.mp3');
+    tick.play();
+    setTimeout(() => {
+        tick.pause();
+    }, 1000);
 }, 1000);
 
 // Function to show clock
@@ -17,7 +24,11 @@ function showClock() {
     let hours = d.getHours();
     let minutes = d.getMinutes();
     let seconds = d.getSeconds();
+    let dateShow = `${d.getDate()}`;
+    let monthShow = `${d.getMonth() + 1}`;
+    let yearShow = `${d.getFullYear() % 2000}`;
     let format = "";
+    let dateStr = "";
 
     // Converting hour into 12 hour format
     if (hours >= 12) {
@@ -36,4 +47,30 @@ function showClock() {
     hour.style.transform = `rotate(${hRot}deg)`;
     minute.style.transform = `rotate(${mRot}deg)`;
     second.style.transform = `rotate(${sRot}deg)`;
+
+    // Converting in 2 digits
+    if (d.getDate() < 10) {
+        dateShow = `0${d.getDate()}`;
+    }
+    if (d.getMonth() < 9) {
+        monthShow = `0${d.getMonth() + 1}`;
+    }
+
+    // Time formatting
+    if (hours == 0) {
+        hours = 12;
+    }
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    if (seconds < 10) {
+        seconds = `0${seconds}`;
+    }
+
+    // Printing date & format
+    dateStr = `<p>${hours}-${minutes}-${seconds} ${format}</p><p>${dateShow}/${monthShow}/${yearShow}</p>`;
+    time.innerHTML = dateStr;
 }
